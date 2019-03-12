6ce5a836ecbcf0bd016a968a868b69d8211824aa
metgemil <metgemil@192.168.213.2>
2019-03-12 22:56:21 +0200

    jni

---
+++ JavaLearn/src/main/java/jni/Calculator.java
@@ -0,0 +1,32 @@
+/*
+ * $Id$
+ */
+package jni;
+
+public class Calculator
+{
+    static
+    {
+        System.loadLibrary("libcalc.so");
+    }
+
+    private Integer mFirstOperand;
+
+    private Integer mSecondOperand;
+
+    private String  mOperator;
+
+    private native int calculate(int firstOp, int secondOp, String operator);
+
+    public Calculator(int firstOp, int secondOp, String operator)
+    {
+        mFirstOperand = firstOp;
+        mSecondOperand = secondOp;
+        mOperator = operator;
+    }
+
+    public Integer getResult()
+    {
+        return calculate(mFirstOperand, mSecondOperand, mOperator);
+    }
+}
---
+++ JavaLearn/src/main/java/jni/JniTest.java
@@ -0,0 +1,41 @@
+/*
+ * $Id$
+ */
+package jni;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JniTest
+{
+    static BufferedReader sReader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args)
+    {
+        System.out.println("Welcome to calculator");
+        System.out.println("provide first operand: ");
+        int firstOp = 0, secondOp = 0;
+        String operator = null;
+        try
+        {
+            String strFirstOp = sReader.readLine();
+            firstOp = Integer.valueOf(strFirstOp);
+
+            System.out.println("provide second operand: ");
+
+            String strSecondOp = sReader.readLine();
+            secondOp = Integer.valueOf(strSecondOp);
+
+            System.out.println("provide operator: ");
+            operator = sReader.readLine();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+
+        Calculator calc = new Calculator(firstOp, secondOp, operator);
+        System.out.println(calc.getResult());
+    }
+}
