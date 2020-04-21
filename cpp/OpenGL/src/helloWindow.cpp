8fc0802bfae3928fbfd4a345c807788745d9c4e8
Metin <metin.gemil@gmail.com>
2020-04-22 01:05:03 +0300

    Cherno OpenGL tutorial

---
+++ cpp/OpenGL/src/helloWindow.cpp
@@ -0,0 +1,33 @@
+/*
+ * helloWindow.cpp
+ *
+ *  Created on: Apr 20, 2020
+ *      Author: zeus
+ */
+
+//#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+//#include <GL/gl.h>
+#include <iostream>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    getchar();
+
+    return 0;
+}
